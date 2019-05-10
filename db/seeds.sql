82d25bce5a213868e724cfd1f4856ee2695017c1
Annie Stewart <anniestewart@Annies-MacBook-Air.local>
2019-05-10 11:40:36 -0400

    mysql setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
